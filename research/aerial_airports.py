dacfe1383360cc173c537ae54c6037ef42aee229
Delaney <delaneyrosario98@gmail.com>
2023-12-13 17:15:43 +0000

    delete venv

---
+++ research/aerial_airports.py
@@ -0,0 +1,16 @@
+OPEN_AQ_API_KEY = "AIzaSyAlvKiF5d2XPTwT1PKpNHd2RUdNXLZxwuI"
+
+
+# -------- Image Setup --------
+zoom = 14
+size = 2048
+scale = 2
+format = "png32"
+# ------------------------------
+
+
+
+
+
+#
+# https://maps.googleapis.com/maps/api/staticmap?center=49.1951,-123.1788&zoom=14&size=2048x2048&scale=2&format=png32&maptype=satellite&key=AIzaSyAlvKiF5d2XPTwT1PKpNHd2RUdNXLZxwuI
