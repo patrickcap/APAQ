eeaf1099c1312dd706328e1af29748257a823b7c
Delaney <delaneyrosario98@gmail.com>
2024-01-02 05:56:57 +0000

    code for dowloading API images

--- research/parsing_latlong.py
+++ research/parsing_latlong.py
@@ -18,20 +18,21 @@ def parsing_latlong(filename) -> pd.DataFrame:
     # calculate latitute and longitude, save airport name
     data["lat_dec"] = lat_degrees + lat_minutes/60 + lat_seconds/(60*60)
     data["long_dec"]  = long_degrees + long_minutes/60 + long_seconds/(60*60)
-    data["airport"] = data[2]
+    data["airport"] = data[0]
 
     #drop null values 
-    longlat_data = data[['airport','lat_dec','long_dec','direction_lat','direction_long']].dropna()
+    longlat_data = data[["airport","lat_dec","long_dec","direction_lat","direction_long"]].dropna()
 
     #drop null directions
     longlat_data = longlat_data[longlat_data.direction_lat != "U"]
     longlat_data = longlat_data[longlat_data.direction_long != "U"]
 
     #add negatives and positives to longitude data based on direction
-    longlat_data.loc[longlat_data['direction_lat'] == "S", 'lat_dec'] = -1*longlat_data["lat_dec"] 
-    longlat_data.loc[longlat_data['direction_long'] == "W", 'long_dec'] = -1*longlat_data["long_dec"] 
+    longlat_data.loc[longlat_data["direction_lat"] == "S", "lat_dec"] = -1*longlat_data["lat_dec"] 
+    longlat_data.loc[longlat_data["direction_long"] == "W", "long_dec"] = -1*longlat_data["long_dec"] 
     longlat_data = longlat_data.drop(["direction_lat","direction_long"], axis=1).reset_index(drop=True)
     
     longlat_data = longlat_data.replace(" ", "_", regex=True)
+    longlat_data = longlat_data.replace("/", "_", regex=True)
 
     return longlat_data
