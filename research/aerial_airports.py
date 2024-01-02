8f63282e4edeee8c3fe9f76dd07c307fe8282fce
Delaney <delaneyrosario98@gmail.com>
2024-01-02 05:56:57 +0000

    code for dowloading API images

--- research/aerial_airports.py
+++ research/aerial_airports.py
@@ -5,7 +5,6 @@ Downloads images of all airports from the Google Static Maps API
 import requests
 from parsing_latlong import parsing_latlong
 
-GOOGLEMAPS_API_KEY = "" #API Key here
 
 # -------- Image Setup --------
 zoom = "15"
@@ -17,30 +16,33 @@ maptype = "satellite"
 
 # -------- File Strings --------
 airport_data = "../data/GlobalAirportDatabase.txt"
-store_folder = "C:/Users/delan/Desktop/APAQ/data/airport_images/"
 # ------------------------------
+
+
+GOOGLEMAPS_API_KEY = input("Enter your Google Static Maps API key: ") 
+store_folder = input("Enter the directory you want to store the images to: ") #C:/Users/delan/Desktop/APAQ/data/airport_images/
 
 latlong_df = parsing_latlong(airport_data) # get the airport dataframe
 
-latlong_df['combined'] = latlong_df[['lat_dec','long_dec']].apply(lambda row: ','.join(row.values.astype(str)), axis=1) # combine lat and long into one string
+latlong_df["combined"] = latlong_df[["lat_dec","long_dec"]].apply(lambda row: ",".join(row.values.astype(str)), axis=1) # combine lat and long into one string
 
 # create latlong url
-latlong_df['url'] = latlong_df['combined'].apply(lambda x: "https://maps.googleapis.com/maps/api/staticmap?center=" + str(x) + "&zoom=" + zoom + "&size=" + size + "&scale=" + scale + "&format=" + format + "&maptype=" + maptype + "&key=" + GOOGLEMAPS_API_KEY)
+latlong_df["url"] = latlong_df["combined"].apply(lambda x: "https://maps.googleapis.com/maps/api/staticmap?center=" + str(x) + "&zoom=" + zoom + "&size=" + size + "&scale=" + scale + "&format=" + format + "&maptype=" + maptype + "&key=" + GOOGLEMAPS_API_KEY)
 
 # create string for filename 
-latlong_df['storage_name'] = latlong_df['airport'].apply(lambda x: store_folder + x + ".png")
-
+latlong_df["storage_name"] = latlong_df["airport"].apply(lambda x: store_folder + x + ".png")
 
 # requests implementation
 def retrieve_images(row) -> None:
     """
     Implements the retrieval of images from given URL to given folder  
     """
-    picture_request = requests.get(row['url'])
+    picture_request = requests.get(row["url"])
     if picture_request.status_code == 200:
-        with open(row['storage_name'], 'wb') as f:
+        with open(row["storage_name"], "wb") as f:
             f.write(picture_request.content)
 
 # apply requests through dataframe of airports 
 latlong_df.apply(lambda row: retrieve_images(row),axis =1)
+
 
